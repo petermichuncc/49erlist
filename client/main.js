0a6f1fa40614f74b354424ec1b132bf8f8e5a8ed
petermichuncc <plarrimo@uncc.edu>
2016-09-09 22:25:28 -0400

    comment for what to do next.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('myApp', ['ngRoute','ngMaterial']);
-
+/*start working on the pages needed for this tutoring app*/
 myApp.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
